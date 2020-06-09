3b537ebef29770af84189971d353a32d73ce220f
Ákos Nikházy <nikhazy.akos@gmail.com>
2020-06-09 13:34:11 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(775, 425);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LOG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 485);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kamera Figyelő";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+
+    public partial class Form1 : Form
+    {
+
+
+
+        Boolean itIsDown = false;
+
+        DateTime blackoutStartTime;
+        DateTime blackoutStopTime;
+
+
+
+        protected void Page_Load()
+        {
+
+
+
+            try
+            {
+                WebClient client = new WebClient();
+                string result = client.DownloadString("YOUR URL TO CHECK");
+
+                if (itIsDown)
+                {
+                    itIsDown = false;
+                    blackoutStopTime = DateTime.Now;
+
+                    textBox1.AppendText("\r\n");
+                    textBox1.AppendText("The cam is active: " + blackoutStopTime + "\r\n");
+
+                    TimeSpan time = TimeSpan.FromSeconds(blackoutStopTime.Subtract(blackoutStartTime).TotalSeconds);
+                    string blackoutTime = time.ToString(@"hh\:mm\:ss");
+
+                    textBox1.AppendText("Downtime: " + blackoutTime);
+                    textBox1.AppendText("\r\n");
+                    textBox1.AppendText("-----------------------------" + "\r\n");
+
+                    File.WriteAllText("log-" + DateTime.Now.ToString("yyyyMMdd") + ".txt", textBox1.Text);
+
+
+                }
+            }
+            catch (WebException ex)
+            {
+                if (!itIsDown)
+                {
+                    blackoutStartTime = DateTime.Now;
+                    itIsDown = true;
+                    textBox1.AppendText("The cam is inactive from " + blackoutStartTime);
+
+                    File.WriteAllText("log-" + DateTime.Now.ToString("yyyyMMdd") + ".txt", textBox1.Text);
+
+
+                }
+
+
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Page_Load();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", "log-" + DateTime.Now.ToString("yyyyMMdd") + ".txt");
+        }
+    }
+}
